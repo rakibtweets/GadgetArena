f6a47feb393b26de7c7848a561ac11c9a0f0019c
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-12-16 22:05:42 +0600

    Add cmdk package, update utils.ts, and modify SearchBar component

--- components/shared/Navbar/SearchBar.tsx
+++ components/shared/Navbar/SearchBar.tsx
@@ -1,22 +1,60 @@
+'use client';
 import { Button } from '@/components/ui/button';
+import { isMacOs } from '@/lib/utils';
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList
+} from '@/components/ui/command';
 import { Search } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 const SearchBar = () => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener('keydown', down);
+    return () => document.removeEventListener('keydown', down);
+  }, []);
   return (
-    <Button
-      variant="outline"
-      className="relative h-9 w-9 p-0 xl:h-10 xl:w-60 xl:justify-start xl:px-3 xl:py-2"
-    >
-      <Search className="h-4 w-4 xl:mr-2" aria-hidden="true" />
-      <span className="hidden xl:inline-flex">Search products...</span>
-      <span className="sr-only">Search products</span>
-      <kbd className="pointer-events-none absolute right-1.5 top-2 hidden h-6 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-xs font-medium opacity-100 xl:flex">
-        <abbr title={'Control'} className="no-underline">
-          Ctrl
-        </abbr>
-        K
-      </kbd>
-    </Button>
+    <>
+      <Button
+        variant="outline"
+        className="relative h-9 w-9 p-0 xl:h-10 xl:w-60 xl:justify-start xl:px-3 xl:py-2"
+        onClick={() => setOpen(true)}
+      >
+        <Search className="h-4 w-4 xl:mr-2" aria-hidden="true" />
+        <span className="hidden xl:inline-flex">Search products...</span>
+        <span className="sr-only">Search products</span>
+        <kbd className="pointer-events-none absolute right-1.5 top-2 hidden h-6 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-xs font-medium opacity-100 xl:flex">
+          <abbr
+            title={isMacOs() ? 'Command' : 'Control'}
+            className="no-underline"
+          >
+            {isMacOs() ? '⌘' : 'Ctrl'}
+          </abbr>
+          K
+        </kbd>
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>Show Product here</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
   );
 };
 export default SearchBar;
