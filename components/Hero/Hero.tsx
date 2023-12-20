af2af540f1f0ad1974d8f95398956570661f21e8
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-12-20 23:35:03 +0600

    Update Hero component height

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import Container from '../ui/Container';
 const Hero = () => {
   return (
     <Container>
-      <div className="bottom-2 flex h-[200px] items-center justify-center border border-red-500">
+      <div className="bottom-2 flex h-[500px] items-center justify-center border border-red-500">
         <h1>Home</h1>
       </div>
     </Container>
