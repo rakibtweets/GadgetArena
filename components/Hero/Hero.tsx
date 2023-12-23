d9342bf242c144d71361b1ef28154ee15ce901e1
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-12-23 23:14:40 +0600

    Refactor Hero component layout and add image

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,10 +1,43 @@
+'use client';
+import Image from 'next/image';
 import Container from '../ui/Container';
+import { Button } from '../ui/button';
+import { ArrowRight } from 'lucide-react';
 
 const Hero = () => {
   return (
     <Container>
-      <div className="bottom-2 flex h-[500px] items-center justify-center border border-red-500">
-        <h1>Home</h1>
+      <div className=" flex h-[500px] w-full items-center justify-center border border-red-500 bg-black p-10 text-white">
+        <div className="flex h-full w-full items-center justify-center border border-red-500">
+          <div className="flex h-full flex-1 flex-col items-start justify-center ps-6  ">
+            <div className="mb-8">
+              <p className="text-center font-normal leading-6">
+                iphone 15 series
+              </p>
+            </div>
+            <h1 className=" mb-4 text-5xl font-semibold leading-snug">
+              Up to 10% <br /> off Voucher
+            </h1>
+
+            <div className="mt-2">
+              <Button
+                className="gap-2 text-foreground"
+                size={'sm'}
+                variant={'secondary'}
+              >
+                Shop Now <ArrowRight />
+              </Button>
+            </div>
+          </div>
+          <div className="flex-1 content-center">
+            <Image
+              src={'/assets/images/hero_iphone.png'}
+              width={500}
+              height={500}
+              alt="hero"
+            />
+          </div>
+        </div>
       </div>
     </Container>
   );
